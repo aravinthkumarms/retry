2f7b24a9df750108024ad0d504751ff9ed58bda1
aravinthkumarms <aravinthkumarse@gmail.com>
2023-06-12 17:47:13 +0530

    controller

--- src/main/java/com/app/retry/RetryApplication.java
+++ src/main/java/com/app/retry/RetryApplication.java
@@ -2,6 +2,8 @@ package com.app.retry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class RetryApplication {
@@ -9,5 +11,6 @@ public class RetryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RetryApplication.class, args);
 	}
+
 
 }
